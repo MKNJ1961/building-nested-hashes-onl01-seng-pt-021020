cf1f7852abc6e3c04d7ee9b8e0561e307097b4e5
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-21 13:12:04 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,13 +7,13 @@ def third_challenge
       :hero_friends => []
    },
    :capulet => {
-      :patriarch => {
-      :name => {:name => "Lord Capulet", :age => "50"},
+      :patriarch => {:name => {:name => "Lord Capulet", :age => "50"},
       :matriarch => {:name => "Lady Capulet", :age => "51"},
       :heroine => {:name => "Juliet", :age => "15", :status => "alive"},
       :heroine_friends => []
    }
-  }
+ }
+  
 
 
 end
